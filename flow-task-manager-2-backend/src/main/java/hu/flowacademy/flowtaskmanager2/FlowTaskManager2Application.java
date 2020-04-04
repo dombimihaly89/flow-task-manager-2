4b1f9224a9fc29d4f29d3696b8908d80a4cf66a1
Mihaly Dombi <dombimihaly89@gmail.com>
2020-04-04 22:31:55 +0200

    Added backend skeleton to the project with basic configuration

---
+++ flow-task-manager-2-backend/src/main/java/hu/flowacademy/flowtaskmanager2/FlowTaskManager2Application.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.flowtaskmanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowTaskManager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowTaskManager2Application.class, args);
+	}
+
+}
---
+++ flow-task-manager-2-backend/src/test/java/hu/flowacademy/flowtaskmanager2/FlowTaskManager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.flowtaskmanager2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTaskManager2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
