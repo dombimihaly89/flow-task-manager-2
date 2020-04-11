df6aa3c994d7124a3a0df935821cb34266c62708
Mihaly Dombi <dombimihaly89@gmail.com>
2020-04-11 23:51:58 +0200

    Added some animation to navbar elements

--- flow-task-manager-2-frontend/src/app/shared/components/header.component.ts
+++ flow-task-manager-2-frontend/src/app/shared/components/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   styles: [
     `
       nav {
@@ -9,10 +9,51 @@ import { Component } from "@angular/core";
         font-size: 1.2em;
       }
 
+      .nav-link:hover {
+        animation-name: grow;
+        animation-duration: 1.5s;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+      }
+
       .navbar-brand {
         padding: 10px;
         border-style: solid;
         border-radius: 50%;
+      }
+
+      .navbar-brand:hover {
+        animation-name: rocking;
+        animation-duration: 5s;
+        animation-iteration-count: infinite;
+        animation-fill-mode: forwards;
+        animation-timing-function: ease-out;
+      }
+
+      .collapsed-item {
+        padding-left: 40px;
+      }
+
+      @keyframes rocking {
+        0% {
+          transform: rotate(0deg);
+        }
+        33% {
+          transform: rotate(15deg);
+        }
+        66% {
+          transform: rotate(-15deg);
+        }
+        100% {
+          transform: rotate(0deg);
+        }
+      }
+
+      @keyframes grow {
+        100% {
+          transform: scale(1.3);
+        }
+      }
       }
     `,
   ],
@@ -33,17 +74,20 @@ import { Component } from "@angular/core";
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarMenu" [ngbCollapse]="navbarCollapsed">
+      <div
+        class="collapse navbar-collapse"
+        id="navbarMenu"
+        [ngbCollapse]="navbarCollapsed"
+      >
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#"
-              >Home</a>
+            <a class="nav-link" [ngClass]="{'collapsed-item': !navbarCollapsed}" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Profile</a>
+            <a class="nav-link" [ngClass]="{'collapsed-item': !navbarCollapsed}" href="#">Profile</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Tasks</a>
+            <a class="nav-link" [ngClass]="{'collapsed-item': !navbarCollapsed}" href="#">Tasks</a>
           </li>
         </ul>
       </div>
