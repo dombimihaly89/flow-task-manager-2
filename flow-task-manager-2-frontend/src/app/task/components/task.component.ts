0f6e2b2095e03b7226aacba55a56e269e231b533
Mihaly Dombi <dombimihaly89@gmail.com>
2020-04-15 01:28:13 +0200

    added horizontal line into cards in task component

--- flow-task-manager-2-frontend/src/app/task/components/task.component.ts
+++ flow-task-manager-2-frontend/src/app/task/components/task.component.ts
@@ -4,64 +4,66 @@ import { TaskService } from "~/app/shared/services/task.service";
 
 @Component({
   selector: "app-task",
-  styles: [`
-    h1 {
-      text-align: center;
-    }
+  styles: [
+    `
+      h1 {
+        text-align: center;
+      }
 
-    .JAVA {
-      background-image: url('../../../assets/images/java.jpg');
-      background-size: cover;
-    }
+      .JAVA {
+        background-image: url("../../../assets/images/java.jpg");
+        background-size: cover;
+      }
 
-    .SPRING {
-      background-image: url('../../../assets/images/spring.png');
-      background-size: cover;
-    }
+      .SPRING {
+        background-image: url("../../../assets/images/spring.png");
+        background-size: cover;
+      }
 
-    .JAVASCRIPT {
-      background-image: url('../../../assets/images/javascript.png');
-      background-size: cover;
-    }
+      .JAVASCRIPT {
+        background-image: url("../../../assets/images/javascript.png");
+        background-size: cover;
+      }
 
-    .mat-card-avatar {
-      height: 60px;
-      width: 60px;
-    }
+      .mat-card-avatar {
+        height: 60px;
+        width: 60px;
+      }
 
-    .mat-card {
-      margin-bottom: 20px;
-    }
+      .mat-card {
+        margin-bottom: 20px;
+      }
 
-    .like {
-      background-color: #0E82CF;
-      color: white;
-    }
+      .like {
+        background-color: #0e82cf;
+        color: white;
+      }
 
-    .dislike {
-      background-color: #EB2805;
-      color: white;
-    }
+      .dislike {
+        background-color: #eb2805;
+        color: white;
+      }
 
-    .post {
-      background-color: #0ECFB4;
-      margin-left: 10px;
-    }
+      .post {
+        background-color: #0ecfb4;
+        margin-left: 10px;
+      }
 
-    .createdAt {
-      float: right;
-      margin-right: 10px;
-    }
-  `],
+      .createdAt {
+        float: right;
+        margin-right: 10px;
+      }
+    `,
+  ],
   template: `
     <h1>Tasks</h1>
     <mat-card *ngFor="let task of tasks" class="example-card">
-      <mat-card-header>
-        <div mat-card-avatar [ngClass]="task.type"></div>
-        <mat-card-title>{{ task.type }}</mat-card-title>
-        <mat-card-subtitle> {{ task. title }}</mat-card-subtitle>
-      </mat-card-header>
-
+        <mat-card-header>
+          <div mat-card-avatar [ngClass]="task.type"></div>
+          <mat-card-title>{{ task.title }}</mat-card-title>
+          <mat-card-subtitle> {{ task.type }}</mat-card-subtitle>
+        </mat-card-header>
+      <hr />
       <mat-card-content>
         <p>
           {{ task.content }}
@@ -71,11 +73,14 @@ import { TaskService } from "~/app/shared/services/task.service";
         <button mat-button class="like">LIKE</button>
         <button mat-button class="dislike">DISLIKE</button>
         <button mat-button class="post">POST A SOLUTION</button>
-        <p>likes: 3, dislikes: 4, </p>
+        <p>likes: 3, dislikes: 4,</p>
       </mat-card-actions>
       <mat-card-footer>
-        <p>Solutions: 4
-        <span class="createdAt"> Created: {{ task.createdAt | date:'yyyy.MM.dd. HH:mm' }} </span>
+        <p>
+          Solutions: 4
+          <span class="createdAt">
+            Created: {{ task.createdAt | date: "yyyy.MM.dd. HH:mm" }}
+          </span>
         </p>
       </mat-card-footer>
     </mat-card>
